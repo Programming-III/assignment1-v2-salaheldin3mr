616e28325140f0741fe443b9965514fbb70b4313
salaheldin3mr <salaheldin3mr@gmail.com>
2025-12-09 09:50:46 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -12,7 +12,7 @@ class person{
   person:person(n,i);
   person::~person();
   void person:: display();
-boolean person:: operator==( a ,  b,  s){
+boolean person:: operator==( a ,  b,  s)
   
 };
 
